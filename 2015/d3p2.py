4f4179e7bee56c012cbc393101f51ac0d715d2d3
mattrcarey <Matthewrcwork@gmail.com>
2021-09-16 11:07:47 -0400

    2015 day3 part2

---
+++ 2015/d3p2.py
@@ -0,0 +1,40 @@
+def main() :
+    f = open("d3i1.txt", "r")
+    data = f.readline()
+    x = 0
+    y = 0
+    roboX = 0
+    roboY = 0
+    houses = 1
+    visited = {(0,0) : 1}
+    count = 0;
+    for i in data :
+        if (count%2 == 0) :
+            if (i=='^') :
+                x+=1
+            elif (i=='v') :
+                x-=1
+            elif (i=='>') :
+                y+=1
+            else :
+                y-=1
+            if (not ((x,y) in visited)) :
+                houses+=1
+                visited[(x,y)] = 1
+        else :
+            if (i=='^') :
+                roboX+=1
+            elif (i=='v') :
+                roboX-=1
+            elif (i=='>') :
+                roboY+=1
+            else :
+                roboY-=1
+            if (not ((roboX,roboY) in visited)) :
+                houses+=1
+                visited[(roboX,roboY)] = 1
+        count+=1
+    print(houses)
+
+if __name__ == "__main__" :
+    main()
