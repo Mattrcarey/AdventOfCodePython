72f15fe3c4a2f5d4cb4777443a94f62f8a8d60a9
mattrcarey <Matthewrcwork@gmail.com>
2021-09-16 09:49:48 -0400

    2015 day2 part1

---
+++ 2015/Day2/d2p1.py
@@ -0,0 +1,17 @@
+import time
+
+def main():
+    f = open("d2i1.txt", "r")
+    total = 0
+    for x in f:
+        data = x.split('x')
+        side1 = int(data[0]) * int(data[1])
+        side2 = int(data[1]) * int(data[2])
+        side3 = int(data[2]) * int(data[0])
+        total += 2 * side1 + 2 * side2 + 2 * side3
+        total += min(min(side1, side2), side3)
+    f.close()
+    print(total)
+ 
+if __name__ == "__main__":  
+    main()
