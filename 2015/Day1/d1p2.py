95791a357b6d445851dc92d30cf46b3b039a64b2
mattrcarey <Matthewrcwork@gmail.com>
2021-09-15 23:46:08 -0400

    2015 day 1 part 2

---
+++ 2015/Day1/d1p2.py
@@ -0,0 +1,19 @@
+def main():
+    f = open("d1i1.txt", "r")
+    line = f.readline()
+    f.close()
+    floor = 0
+    count = 1
+    for x in line:
+        if (x == '('):
+            floor+=1
+        elif (x == ')'):
+            floor-=1
+        if (floor == -1):
+            print(count)
+            break
+        count+=1
+
+
+if __name__ == "__main__":
+    main()
