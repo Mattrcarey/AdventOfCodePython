8fdb44b61bb24d092c61193b0fccee344bbd6450
mattrcarey <Matthewrcwork@gmail.com>
2021-09-15 23:43:17 -0400

    Whoops forgot to close the file

--- 2015/Day1/d1p1.py
+++ 2015/Day1/d1p1.py
@@ -1,12 +1,14 @@
 def main():
     f = open("d1i1.txt", "r")
     line = f.readline()
+    f.close()
     count = 0
     for x in line:
         if (x == '('):
             count+=1
         elif (x== ')'):
             count-=1
+    
     print(count)
 
 
