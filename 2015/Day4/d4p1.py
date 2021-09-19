17d13570d22269acba0c48a9d7c4f3a74116721b
mattrcarey <Matthewrcwork@gmail.com>
2021-09-18 22:50:34 -0400

    2015 day4 part1

---
+++ 2015/Day4/d4p1.py
@@ -0,0 +1,19 @@
+import hashlib
+
+
+def main():
+    message = "iwrupvqb"
+    count = 1
+
+    while (1):
+        message2 = message + str(count)
+        result = hashlib.md5(message2.encode())
+        result = result.hexdigest()
+        if(result[0 : 5] == "00000") :
+            print(count)
+            break
+        count+=1
+
+
+if __name__ == "__main__" :
+    main()
