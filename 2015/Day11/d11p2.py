be47a196c2bc530c600539be98b0be6546d7ccab
mattrcarey <Matthewrcwork@gmail.com>
2021-10-18 04:00:06 -0400

    2015 Day 11 Part 1&2

---
+++ 2015/Day11/d11p2.py
@@ -0,0 +1,35 @@
+import re
+import sys
+
+def main():
+    p = sys.argv[1]
+    valid = False
+    while not valid:
+        rev = list(p)[::-1]
+        i = 0
+        for x in rev:                   # increment the string
+            if x == 'z':
+                rev[i] = 'a'
+            else:
+                rev[i] = chr(ord(x)+1)
+                break
+            i += 1
+        p = ''.join(rev[::-1])
+            
+        increment = False 
+        for i in range(len(p) - 2):     # check if the string is valid
+            if (ord(p[i]) == ord(p[i+1])-1 and ord(p[i]) == ord(p[i+2])-2):
+                increment = True
+                break
+        if (not increment):                         # doesn't have an increment
+            continue
+        if ('i' in p or 'o' in p or 'l' in p):      # has an illegal char
+            continue
+        if len(set(re.findall(r'(.)\1', p))) < 2:   # doesn't have 2 doubles 
+            continue
+        valid = True
+    print (p)
+
+
+if __name__ == "__main__":
+    main()
